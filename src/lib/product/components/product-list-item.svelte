fc06620811b6a8d72e909130e8aa9bc748c5dfca
Param <param@mac.com>
2025-02-21 10:14:02 +0530

    Updated cart

--- src/lib/product/components/product-list-item.svelte
+++ src/lib/product/components/product-list-item.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { Button } from '@cloudparker/moldex.js';
+	import { Button, TextCurrency } from '@cloudparker/moldex.js';
 	import type { ProductDataModel } from '../product-type';
 	import ButtonIncrement from '$lib/core/components/button-increment.svelte';
 
@@ -14,6 +14,14 @@
 <div class="flex gap-4">
 	<div class="flex-grow">
 		<div class="font-bold">{product?.name}</div>
+		<div class="flex items-center gap-2">
+			<span class="text-sm font-bold text-primary">
+				<TextCurrency input={product.salesPrice || 0} symbol="Rs." hasSymbol />
+			</span>
+			<span class="text-base-400 text-sm font-thin line-through">
+				<TextCurrency input={product.mrp || 0} symbol="Rs." hasSymbol />
+			</span>
+		</div>
 		<div class="text-sm font-thin text-base">{product.desc}</div>
 	</div>
 	<div>
